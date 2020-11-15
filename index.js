4e29952aaf36421e22c6cf8ca3027cd5f258eb5b
Gabriel <aballaygabriel@gmail.com>
2020-11-15 04:30:51 -0300

    error

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.use( express.static( publicPath ) );
 
 
 
-server.listen( process.env.PORT, ( err ) => {
+server.listen(process.env.PORT || 3000, ( err ) => {
 
     if ( err ) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT );
+    console.log('Servidor corriendo en puerto', process.env.PORT || 3000);
 
 });
 
